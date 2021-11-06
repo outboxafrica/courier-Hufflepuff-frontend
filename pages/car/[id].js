6b6c6c879815aa46636db96bc55b79da8b70e6af
diphan-source <ahabwediphan@yahoo.com>
2021-11-06 18:38:15 +0000

    ft-add detailspage

---
+++ pages/car/[id].js
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import { server } from "../../config/config";
+import styles from "../../styles/Cart.module.css";
+import QuantityBtn from "../../components/QuantityBtn";
+import {useState} from 'react';
+
+function Singlecar({ item }) {
+ 
+//   console.log({ total, cart ,removeFromCart});
+  const [quantity, setQuantity] = useState(0);
+
+  const addToCartHandler = async () => {
+
+    addToCart(item, quantity);
+    //set item and quantity to localstorage
+    
+  };
+
+  
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={cart.img}>
+          <Image src={item.image} width={600} height={600} alt={item?.product} />
+        </div>
+        <div className={cart.content}>
+          <h2>{item?.product}</h2>
+          <p>{item?.desc}</p>
+          <span>{item?.price}</span>
+          <span>{item?.Mileage}</span>
+          <div>
+            <QuantityBtn quantity={quantity} setQuantity={setQuantity}/>
+            <button onClick={addToCartHandler}>ADD To Cart</button>
+          </div>
+        </div>
+
+        {/* <p>{item.}</p> */}
+      </div>
+    </>
+  );
+}
+
+export default Singlecar;
+
+export async function getStaticProps(context) {
+  const res = await fetch(`${server}/car/${context.params.id}`);
+  const data = await res.json();
+
+  return { props: { item: data } };
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(server);
+  const { data } = await res.json();
+
+  const paths = data?.map((item) => ({
+    params: { id: item?.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+}
