45f5bea62956cd2818605e951b5c97b9503b5acb
diphan-source <ahabwediphan@yahoo.com>
2021-11-08 05:06:59 +0000

    fix-url

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 export const server =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://car-commerce.vercel.app/api";
+    : "https://hufflepuff.vercel.app/api";
 
    
      // Import the functions you need from the SDKs you need
